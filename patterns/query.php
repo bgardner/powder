237f0ba03f9ea23e17e74c557be544fb83b7e675
Brian Gardner <bmg1227@gmail.com>
2022-11-23 20:57:22 -0600

    Create Query pattern, consolidate templates.

---
+++ patterns/query.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Title: Query of posts.
+ * Slug: powder/query
+ * Categories: hidden
+ * Inserter: false
+ */
+?>
+<!-- wp:query {"queryId":0,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","type":"constrained","inherit":true,"perPage":1},"displayLayout":{"type":"list"},"layout":{"type":"default"}} -->
+<div class="wp-block-query">
+<!-- wp:post-template -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"10px"}}} -->
+<div class="wp-block-group">
+<!-- wp:post-title {"isLink":true} /-->
+<!-- wp:group {"layout":{"type":"flex"},"style":{"spacing":{"blockGap":"5px","margin":{"bottom":"30px"}}},"fontSize":"x-small"} -->
+<div class="wp-block-group has-x-small-font-size" style="margin-bottom:30px">
+<!-- wp:post-date /-->
+<!-- wp:paragraph -->
+<p>·</p>
+<!-- /wp:paragraph -->
+<!-- wp:post-author {"showAvatar":false} /-->
+</div>
+<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
+<!-- wp:post-content /-->
+<!-- /wp:post-template -->
+<!-- wp:query-pagination -->
+<!-- wp:query-pagination-previous /-->
+<!-- wp:query-pagination-next /-->
+<!-- /wp:query-pagination -->
+</div>
+<!-- /wp:query -->
