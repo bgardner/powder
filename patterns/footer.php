46690ab937d0885d9c5a41893cf86a25f22aeaa5
Brian Gardner <bmg1227@gmail.com>
2023-01-05 08:31:47 -0600

    Theme.json updates, remove margin in footer.

--- patterns/footer.php
+++ patterns/footer.php
@@ -7,8 +7,8 @@
  * Viewport Width: 1280
  */
 ?>
-<!-- wp:group {"layout":{"type":"constrained"},"align":"full","style":{"spacing":{"padding":{"top":"30px","bottom":"30px"},"margin":{"top":"0px"}}},"fontSize":"x-small"} -->
-<div class="wp-block-group alignfull has-x-small-font-size" style="margin-top:0px;padding-top:30px;padding-bottom:30px">
+<!-- wp:group {"layout":{"type":"constrained"},"align":"full","style":{"spacing":{"padding":{"top":"30px","bottom":"30px"}}},"fontSize":"x-small"} -->
+<div class="wp-block-group alignfull has-x-small-font-size" style="padding-top:30px;padding-bottom:30px">
 <!-- wp:group {"layout":{"type":"flex","allowOrientation":false,"justifyContent":"space-between"},"align":"wide","style":{"spacing":{"blockGap":"0px"}}} -->
 <div class="wp-block-group alignwide">
 <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"},"style":{"spacing":{"blockGap":"5px"}}} -->
