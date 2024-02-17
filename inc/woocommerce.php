1c39ed42fbee6fc6e7942fc063b3732ba81235f1
Brian Gardner <bmg1227@gmail.com>
2024-02-17 14:21:22 -0600

    Get Powder theme version for WooCommerce.

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -12,13 +12,19 @@
  * Enqueue WooCommerce styles.
  */
 function powder_enqueue_woocommerce_styles() {
-
- 	// Enqueue WooCommerce style sheet if plugin is active and on a WooCommerce page.
+	// Check if WooCommerce is active and on a WooCommerce page.
 	if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 		if ( is_woocommerce() ) {
-			wp_enqueue_style( 'powder-woocommerce', get_template_directory_uri() . '/assets/css/woocommerce.css' );
+			// Get the Powder theme object.
+			$powder_theme = wp_get_theme( 'powder' );
+
+			// Get the version of the Powder theme.
+			$powder_version = $powder_theme->get( 'Version' );
+
+			// Enqueue the WooCommerce stylesheet.
+			wp_enqueue_style( 'powder-woocommerce', get_template_directory_uri() . '/assets/css/woocommerce.css', array(), $powder_version );
 		}
 	}
-
 }
+
 add_action( 'wp_enqueue_scripts', 'powder_enqueue_woocommerce_styles' );
