d5ce9895be342c330e05e363dbbbc5c1b1a95f45
Brian Gardner <bmg1227@gmail.com>
2024-03-31 08:43:01 -0500

    Update alt tag in mega Footer patterns.

--- patterns/footer-mega.php
+++ patterns/footer-mega.php
@@ -11,7 +11,7 @@
 	<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"10px","padding":{"bottom":"var(--wp--preset--spacing--medium)"}}}} -->
 	<div class="wp-block-group alignwide" style="padding-bottom:var(--wp--preset--spacing--medium)">
 		<!-- wp:image {"align":"center","width":40,"height":40,"sizeSlug":"full","linkDestination":"custom"} -->
-		<figure class="wp-block-image aligncenter size-full is-resized"><a href="https://powder.design/"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/site-logo-dark.svg'; ?>" alt="Site Logo" width="40" height="40"/></a></figure>
+		<figure class="wp-block-image aligncenter size-full is-resized"><a href="https://powder.design/"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/site-logo-dark.svg'; ?>" alt="Powder site logo" width="40" height="40"/></a></figure>
 		<!-- /wp:image -->
 		<!-- wp:paragraph {"align":"center"} -->
 		<p class="has-text-align-center"><?php echo esc_html__( 'Made with Powder', 'powder' ); ?></p>
