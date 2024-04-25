4e5bf9eeff146f49088bb829eb5e01f76440682c
Brian Gardner <bmg1227@gmail.com>
2024-04-24 20:50:39 -0500

    Update Team patterns.

--- patterns/team-4-columns.php
+++ patterns/team-4-columns.php
@@ -12,112 +12,89 @@
 	<!-- /wp:heading -->
 	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|small"},"margin":{"top":"var:preset|spacing|medium"}}}} -->
 	<div class="wp-block-columns alignwide" style="margin-top:var(--wp--preset--spacing--medium)">
-		<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}}} -->
+		<!-- wp:column -->
 		<div class="wp-block-column">
 			<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
 			<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/sample-image-dark.svg'; ?>" alt="Sample image"/></figure>
 			<!-- /wp:image -->
-			<!-- wp:heading {"textAlign":"center","style":{"typography":{"lineHeight":"1.25"}},"fontSize":"medium"} -->
-			<h2 class="wp-block-heading has-text-align-center has-medium-font-size" style="line-height:1.25"><?php echo esc_html__( 'Jennifer Kayne', 'powder' ); ?></h2>
+			<!-- wp:heading {"textAlign":"center","fontSize":"medium"} -->
+			<h2 class="wp-block-heading has-text-align-center has-medium-font-size"><?php echo esc_html__( 'Jennifer Kayne', 'powder' ); ?></h2>
 			<!-- /wp:heading -->
 			<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"x-small"} -->
 			<p class="has-text-align-center has-x-small-font-size" style="margin-top:0"><?php echo esc_html__( 'Chief Executive Officer', 'powder' ); ?></p>
 			<!-- /wp:paragraph -->
+			<!-- wp:social-links {"iconColor":"contrast","iconColorValue":"#0a0a0a","openInNewTab":true,"size":"has-small-icon-size","align":"center","style":{"spacing":{"blockGap":{"left":"5px"},"margin":{"top":"15px"}}},"className":"is-style-outline"} -->
+			<ul class="wp-block-social-links aligncenter has-small-icon-size has-icon-color is-style-outline" style="margin-top:15px">
+				<!-- wp:social-link {"url":"https://twitter.com/","service":"x"} /-->
+				<!-- wp:social-link {"url":"https://www.linkedin.com/","service":"linkedin"} /-->
+				<!-- wp:social-link {"url":"https://instagram.com/","service":"instagram"} /-->
+				<!-- wp:social-link {"url":"https://www.facebook.com/","service":"facebook"} /-->
+			</ul>
+			<!-- /wp:social-links -->
 		</div>
 		<!-- /wp:column -->
-		<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}}} -->
+		<!-- wp:column -->
 		<div class="wp-block-column">
 			<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
 			<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/sample-image-dark.svg'; ?>" alt="Sample image"/></figure>
 			<!-- /wp:image -->
-			<!-- wp:heading {"textAlign":"center","style":{"typography":{"lineHeight":"1.25"}},"fontSize":"medium"} -->
-			<h2 class="wp-block-heading has-text-align-center has-medium-font-size" style="line-height:1.25"><?php echo esc_html__( 'Anthony Hawk', 'powder' ); ?></h2>
+			<!-- wp:heading {"textAlign":"center","fontSize":"medium"} -->
+			<h2 class="wp-block-heading has-text-align-center has-medium-font-size"><?php echo esc_html__( 'Anthony Hawk', 'powder' ); ?></h2>
 			<!-- /wp:heading -->
 			<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"x-small"} -->
 			<p class="has-text-align-center has-x-small-font-size" style="margin-top:0"><?php echo esc_html__( 'Chief Operations Officer', 'powder' ); ?></p>
 			<!-- /wp:paragraph -->
+			<!-- wp:social-links {"iconColor":"contrast","iconColorValue":"#0a0a0a","openInNewTab":true,"size":"has-small-icon-size","align":"center","style":{"spacing":{"blockGap":{"left":"5px"},"margin":{"top":"15px"}}},"className":"is-style-outline"} -->
+			<ul class="wp-block-social-links aligncenter has-small-icon-size has-icon-color is-style-outline" style="margin-top:15px">
+				<!-- wp:social-link {"url":"https://twitter.com/","service":"x"} /-->
+				<!-- wp:social-link {"url":"https://www.linkedin.com/","service":"linkedin"} /-->
+				<!-- wp:social-link {"url":"https://instagram.com/","service":"instagram"} /-->
+				<!-- wp:social-link {"url":"https://www.facebook.com/","service":"facebook"} /-->
+			</ul>
+			<!-- /wp:social-links -->
+
 		</div>
 		<!-- /wp:column -->
-		<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}}} -->
+		<!-- wp:column -->
 		<div class="wp-block-column">
 			<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
 			<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/sample-image-dark.svg'; ?>" alt="Sample image"/></figure>
 			<!-- /wp:image -->
-			<!-- wp:heading {"textAlign":"center","style":{"typography":{"lineHeight":"1.25"}},"fontSize":"medium"} -->
-			<h2 class="wp-block-heading has-text-align-center has-medium-font-size" style="line-height:1.25"><?php echo esc_html__( 'Kya Strickland', 'powder' ); ?></h2>
+			<!-- wp:heading {"textAlign":"center","fontSize":"medium"} -->
+			<h2 class="wp-block-heading has-text-align-center has-medium-font-size"><?php echo esc_html__( 'Autumn Cole', 'powder' ); ?></h2>
 			<!-- /wp:heading -->
 			<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"x-small"} -->
 			<p class="has-text-align-center has-x-small-font-size" style="margin-top:0"><?php echo esc_html__( 'Chief Marketing Officer', 'powder' ); ?></p>
 			<!-- /wp:paragraph -->
+			<!-- wp:social-links {"iconColor":"contrast","iconColorValue":"#0a0a0a","openInNewTab":true,"size":"has-small-icon-size","align":"center","style":{"spacing":{"blockGap":{"left":"5px"},"margin":{"top":"15px"}}},"className":"is-style-outline"} -->
+			<ul class="wp-block-social-links aligncenter has-small-icon-size has-icon-color is-style-outline" style="margin-top:15px">
+				<!-- wp:social-link {"url":"https://twitter.com/","service":"x"} /-->
+				<!-- wp:social-link {"url":"https://www.linkedin.com/","service":"linkedin"} /-->
+				<!-- wp:social-link {"url":"https://instagram.com/","service":"instagram"} /-->
+				<!-- wp:social-link {"url":"https://www.facebook.com/","service":"facebook"} /-->
+			</ul>
+			<!-- /wp:social-links -->
 		</div>
 		<!-- /wp:column -->
-		<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}}} -->
+		<!-- wp:column -->
 		<div class="wp-block-column">
 			<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
 			<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/sample-image-dark.svg'; ?>" alt="Sample image"/></figure>
 			<!-- /wp:image -->
-			<!-- wp:heading {"textAlign":"center","style":{"typography":{"lineHeight":"1.25"}},"fontSize":"medium"} -->
-			<h2 class="wp-block-heading has-text-align-center has-medium-font-size" style="line-height:1.25"><?php echo esc_html__( 'Noah Stone', 'powder' ); ?></h2>
+			<!-- wp:heading {"textAlign":"center","fontSize":"medium"} -->
+			<h2 class="wp-block-heading has-text-align-center has-medium-font-size"><?php echo esc_html__( 'Zach Marley', 'powder' ); ?></h2>
 			<!-- /wp:heading -->
 			<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"x-small"} -->
 			<p class="has-text-align-center has-x-small-font-size" style="margin-top:0"><?php echo esc_html__( 'Chief Financial Officer', 'powder' ); ?></p>
 			<!-- /wp:paragraph -->
-		</div>
-		<!-- /wp:column -->
-	</div>
-	<!-- /wp:columns -->
-	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|small"},"margin":{"top":"var:preset|spacing|medium"}}}} -->
-	<div class="wp-block-columns alignwide" style="margin-top:var(--wp--preset--spacing--medium)">
-		<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}}} -->
-		<div class="wp-block-column">
-			<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
-			<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/sample-image-dark.svg'; ?>" alt="Sample image"/></figure>
-			<!-- /wp:image -->
-			<!-- wp:heading {"textAlign":"center","style":{"typography":{"lineHeight":"1.25"}},"fontSize":"medium"} -->
-			<h2 class="wp-block-heading has-text-align-center has-medium-font-size" style="line-height:1.25"><?php echo esc_html__( 'Thomas Everett', 'powder' ); ?></h2>
-			<!-- /wp:heading -->
-			<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"x-small"} -->
-			<p class="has-text-align-center has-x-small-font-size" style="margin-top:0"><?php echo esc_html__( 'Chief Technology Officer', 'powder' ); ?></p>
-			<!-- /wp:paragraph -->
-		</div>
-		<!-- /wp:column -->
-		<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}}} -->
-		<div class="wp-block-column">
-			<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
-			<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/sample-image-dark.svg'; ?>" alt="Sample image"/></figure>
-			<!-- /wp:image -->
-			<!-- wp:heading {"textAlign":"center","style":{"typography":{"lineHeight":"1.25"}},"fontSize":"medium"} -->
-			<h2 class="wp-block-heading has-text-align-center has-medium-font-size" style="line-height:1.25"><?php echo esc_html__( 'Autumn Cole', 'powder' ); ?></h2>
-			<!-- /wp:heading -->
-			<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"x-small"} -->
-			<p class="has-text-align-center has-x-small-font-size" style="margin-top:0"><?php echo esc_html__( 'Chief Product Officer', 'powder' ); ?></p>
-			<!-- /wp:paragraph -->
-		</div>
-		<!-- /wp:column -->
-		<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}}} -->
-		<div class="wp-block-column">
-			<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
-			<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/sample-image-dark.svg'; ?>" alt="Sample image"/></figure>
-			<!-- /wp:image -->
-			<!-- wp:heading {"textAlign":"center","style":{"typography":{"lineHeight":"1.25"}},"fontSize":"medium"} -->
-			<h2 class="wp-block-heading has-text-align-center has-medium-font-size" style="line-height:1.25"><?php echo esc_html__( 'Zach Marley', 'powder' ); ?></h2>
-			<!-- /wp:heading -->
-			<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"x-small"} -->
-			<p class="has-text-align-center has-x-small-font-size" style="margin-top:0"><?php echo esc_html__( 'Chief Security Officer', 'powder' ); ?></p>
-			<!-- /wp:paragraph -->
-		</div>
-		<!-- /wp:column -->
-		<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}}} -->
-		<div class="wp-block-column">
-			<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
-			<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/sample-image-dark.svg'; ?>" alt="Sample image"/></figure>
-			<!-- /wp:image -->
-			<!-- wp:heading {"textAlign":"center","style":{"typography":{"lineHeight":"1.25"}},"fontSize":"medium"} -->
-			<h2 class="wp-block-heading has-text-align-center has-medium-font-size" style="line-height:1.25"><?php echo esc_html__( 'Kate Reeser', 'powder' ); ?></h2>
-			<!-- /wp:heading -->
-			<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"x-small"} -->
-			<p class="has-text-align-center has-x-small-font-size" style="margin-top:0"><?php echo esc_html__( 'Chief Brand Officer', 'powder' ); ?></p>
-			<!-- /wp:paragraph -->
+			<!-- wp:social-links {"iconColor":"contrast","iconColorValue":"#0a0a0a","openInNewTab":true,"size":"has-small-icon-size","align":"center","style":{"spacing":{"blockGap":{"left":"5px"},"margin":{"top":"15px"}}},"className":"is-style-outline"} -->
+			<ul class="wp-block-social-links aligncenter has-small-icon-size has-icon-color is-style-outline" style="margin-top:15px">
+				<!-- wp:social-link {"url":"https://twitter.com/","service":"x"} /-->
+				<!-- wp:social-link {"url":"https://www.linkedin.com/","service":"linkedin"} /-->
+				<!-- wp:social-link {"url":"https://instagram.com/","service":"instagram"} /-->
+				<!-- wp:social-link {"url":"https://www.facebook.com/","service":"facebook"} /-->
+			</ul>
+			<!-- /wp:social-links -->
 		</div>
 		<!-- /wp:column -->
 	</div>
