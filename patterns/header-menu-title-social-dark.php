4f42653a4fa3eaf7e8a2c47027a8bbe23f293db4
Brian Gardner <bmg1227@gmail.com>
2024-04-28 10:35:01 -0500

    Update Header patterns.

--- patterns/header-menu-title-social-dark.php
+++ patterns/header-menu-title-social-dark.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Title: Header with social icons, site logo
- * Slug: powder/header-logo-center-dark
+ * Title: Header with menu, site title, social icons
+ * Slug: powder/header-menu-title-social-dark
  * Categories: header
  * Block Types: core/template-part/header
  */
@@ -10,8 +10,10 @@
 <div class="wp-block-group alignfull has-base-color has-contrast-background-color has-text-color has-background has-link-color" style="margin-top:0px;padding-top:var(--wp--preset--spacing--x-small);padding-right:30px;padding-bottom:var(--wp--preset--spacing--x-small);padding-left:30px">
 	<!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
 	<div class="wp-block-group alignwide">
-		<!-- wp:social-links {"iconColor":"contrast","iconColorValue":"#0a0a0a","iconBackgroundColor":"base","iconBackgroundColorValue":"#ffffff","openInNewTab":true,"size":"has-small-icon-size","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|x-small","left":"5px"}},"layout":{"selfStretch":"fixed","flexSize":"300px"}},"className":"is-style-hidden-mobile"} -->
-		<ul class="wp-block-social-links has-small-icon-size has-icon-color has-icon-background-color is-style-hidden-mobile">
+		<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true},"style":{"typography":{"textTransform":"uppercase","fontSize":"12px","fontStyle":"normal","fontWeight":"400"},"layout":{"selfStretch":"fixed","flexSize":"300px"}}} /-->		
+		<!-- wp:site-title {"level":0,"style":{"typography":{"textTransform":"uppercase"}},"fontSize":"small"} /-->
+		<!-- wp:social-links {"openInNewTab":true,"size":"has-small-icon-size","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|x-small","left":"5px"}},"layout":{"selfStretch":"fixed","flexSize":"300px"}},"className":"is-style-hidden-mobile is-style-outline","layout":{"type":"flex","justifyContent":"right"}} -->
+		<ul class="wp-block-social-links has-small-icon-size is-style-hidden-mobile is-style-outline">
 			<!-- wp:social-link {"url":"https://twitter.com/","service":"x"} /-->
 			<!-- wp:social-link {"url":"https://www.linkedin.com/","service":"linkedin"} /-->
 			<!-- wp:social-link {"url":"https://instagram.com/","service":"instagram"} /-->
@@ -19,8 +21,6 @@
 			<!-- wp:social-link {"url":"https://www.threads.net/","service":"threads"} /-->
 		</ul>
 		<!-- /wp:social-links -->
-		<!-- wp:site-title {"level":0,"style":{"typography":{"textTransform":"uppercase"}},"fontSize":"small"} /-->
-		<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"},"style":{"typography":{"textTransform":"uppercase","fontSize":"12px","fontStyle":"normal","fontWeight":"400"},"layout":{"selfStretch":"fixed","flexSize":"300px"}}} /-->
 	</div>
 	<!-- /wp:group -->
 </div>
