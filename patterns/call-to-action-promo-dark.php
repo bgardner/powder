e10c2ee508b0fb209b44037de1fa2bc39ce9d4dc
Brian Gardner <bmg1227@gmail.com>
2024-06-19 09:50:17 -0500

    Update Call to Action patterns copy.

--- patterns/call-to-action-promo-dark.php
+++ patterns/call-to-action-promo-dark.php
@@ -8,7 +8,7 @@
 <!-- wp:group {"metadata":{"name":"Call to Action"},"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"margin":{"top":"0"},"padding":{"top":"10px","bottom":"10px","left":"30px","right":"30px"}}},"backgroundColor":"contrast","textColor":"base","fontSize":"x-small","layout":{"type":"default"}} -->
 <div class="wp-block-group alignfull has-base-color has-contrast-background-color has-text-color has-background has-link-color has-x-small-font-size" style="margin-top:0;padding-top:10px;padding-right:30px;padding-bottom:10px;padding-left:30px">
 	<!-- wp:paragraph {"align":"center","fontSize":"x-small"} -->
-	<p class="has-text-align-center has-x-small-font-size"><?php echo esc_html__( 'Powder is the premier WordPress block theme.', 'powder' ); ?> <a href="#"><?php echo esc_html__( 'Get All-Access →', 'powder' ); ?></a></p>
+	<p class="has-text-align-center has-x-small-font-size"><?php echo esc_html__( 'Powder is the premier block theme for WordPress.', 'powder' ); ?> <a href="#"><?php echo esc_html__( 'Learn More →', 'powder' ); ?></a></p>
 	<!-- /wp:paragraph -->
 </div>
 <!-- /wp:group -->
--- patterns/call-to-action-promo.php
+++ patterns/call-to-action-promo.php
@@ -8,7 +8,7 @@
 <!-- wp:group {"metadata":{"name":"Call to Action"},"align":"full","style":{"spacing":{"margin":{"top":"0"},"padding":{"top":"10px","bottom":"10px","left":"30px","right":"30px"}}},"fontSize":"x-small","layout":{"type":"default"}} -->
 <div class="wp-block-group alignfull has-x-small-font-size" style="margin-top:0;padding-top:10px;padding-right:30px;padding-bottom:10px;padding-left:30px">
 	<!-- wp:paragraph {"align":"center","fontSize":"x-small"} -->
-	<p class="has-text-align-center has-x-small-font-size"><?php echo esc_html__( 'Powder is the premier WordPress block theme.', 'powder' ); ?> <a href="#"><?php echo esc_html__( 'Get All-Access →', 'powder' ); ?></a></p>
+	<p class="has-text-align-center has-x-small-font-size"><?php echo esc_html__( 'Powder is the premier block theme for WordPress.', 'powder' ); ?> <a href="#"><?php echo esc_html__( 'Learn More →', 'powder' ); ?></a></p>
 	<!-- /wp:paragraph -->
 </div>
 <!-- /wp:group -->
