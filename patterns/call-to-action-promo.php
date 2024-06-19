7c634ba15cfd73b0480e17df34abee4f025c2a6e
Brian Gardner <bmg1227@gmail.com>
2024-06-19 09:36:38 -0500

    Add Call to Action patterns.

---
+++ patterns/call-to-action-promo.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Call to action with text, link
+ * Slug: powder/call-to-action-promo
+ * Categories: powder-call-to-action
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Call to Action"},"align":"full","style":{"spacing":{"margin":{"top":"0"},"padding":{"top":"10px","bottom":"10px","left":"30px","right":"30px"}}},"fontSize":"x-small","layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull has-x-small-font-size" style="margin-top:0;padding-top:10px;padding-right:30px;padding-bottom:10px;padding-left:30px">
+	<!-- wp:paragraph {"align":"center","fontSize":"x-small"} -->
+	<p class="has-text-align-center has-x-small-font-size"><?php echo esc_html__( 'Powder is the premier WordPress block theme.', 'powder' ); ?> <a href="#"><?php echo esc_html__( 'Get All-Access →', 'powder' ); ?></a></p>
+	<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
