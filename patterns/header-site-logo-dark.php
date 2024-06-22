bf4b69228e6d65b48fad75c681d2ca3016baa710
Brian Gardner <bmg1227@gmail.com>
2024-06-22 15:25:22 -0500

    Add Header patterns.

---
+++ patterns/header-site-logo-dark.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Header with site logo
+ * Slug: powder/header-site-logo-dark
+ * Categories: header
+ * Block Types: core/template-part/header
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0px"},"padding":{"top":"30px","right":"30px","bottom":"30px","left":"30px"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"contrast","textColor":"base","layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-base-color has-contrast-background-color has-text-color has-background has-link-color" style="margin-top:0px;padding-top:30px;padding-right:30px;padding-bottom:30px;padding-left:30px">
+	<!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
+	<div class="wp-block-group alignwide">
+		<!-- wp:group {"style":{"spacing":{"blockGap":"10px"},"layout":{"selfStretch":"fixed","flexSize":"200px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+		<div class="wp-block-group">
+			<!-- wp:site-logo {"width":30} /-->
+			<!-- wp:site-title {"level":0} /-->
+		</div>
+		<!-- /wp:group -->
+		<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true}} /-->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
