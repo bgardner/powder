3ad5d1c901f93cf8f8b60348f31b5bd0f34c9040
Brian Gardner <bmg1227@gmail.com>
2024-07-05 10:42:34 -0500

    Remove .is-style-pull-100 utility class.

--- patterns/template-page-box-shadow.php
+++ patterns/template-page-box-shadow.php
@@ -14,15 +14,15 @@
 		<!-- wp:post-title {"textAlign":"center","level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"300","fontSize":"48px"}}} /-->
 	</header>
 	<!-- /wp:group -->
-	<!-- wp:group {"align":"full","style":{"dimensions":{"minHeight":"100px"},"spacing":{"margin":{"top":"0"}}},"backgroundColor":"contrast","layout":{"type":"constrained"}} -->
-	<div class="wp-block-group alignfull has-contrast-background-color has-background" style="min-height:100px;margin-top:0"></div>
+	<!-- wp:group {"align":"full","style":{"dimensions":{"minHeight":"80px"},"spacing":{"margin":{"top":"0"}}},"backgroundColor":"contrast","layout":{"type":"constrained"}} -->
+	<div class="wp-block-group alignfull has-contrast-background-color has-background" style="min-height:80px;margin-top:0"></div>
 	<!-- /wp:group -->
-	<!-- wp:group {"tagName":"main","style":{"spacing":{"margin":{"top":"0"},"padding":{"left":"30px","right":"30px"}}},"layout":{"type":"constrained","wideSize":"960px"}} -->
-	<main class="wp-block-group" style="margin-top:0;padding-right:30px;padding-left:30px">
+	<!-- wp:group {"tagName":"main","style":{"spacing":{"margin":{"top":"0"}}},"layout":{"type":"constrained","wideSize":"960px"}} -->
+	<main class="wp-block-group" style="margin-top:0">
 		<!-- wp:group {"tagName":"article","layout":{"type":"default"}} -->
 		<article class="wp-block-group">
-			<!-- wp:group {"backgroundColor":"base","className":"is-style-position-relative is-style-positive-zindex is-style-pull-100 is-style-shadow-light","layout":{"type":"default"}} -->
-			<div class="wp-block-group is-style-position-relative is-style-positive-zindex is-style-pull-100 is-style-shadow-light has-base-background-color has-background">
+			<!-- wp:group {"className":"is-style-position-relative is-style-positive-zindex is-style-shadow-light","style":{"spacing":{"margin":{"top":"-80px"}}},"backgroundColor":"base","layout":{"type":"default"}} -->
+			<div class="wp-block-group is-style-position-relative is-style-positive-zindex is-style-shadow-light has-base-background-color has-background" style="margin-top:-80px">
 				<!-- wp:post-featured-image {"aspectRatio":"16/9"} /-->
 				<!-- wp:group {"style":{"spacing":{"margin":{"top":"0px"},"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
 				<div class="wp-block-group" style="margin-top:0px;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--40)">
@@ -35,8 +35,8 @@
 		<!-- /wp:group -->
 	</main>
 	<!-- /wp:group -->
-	<!-- wp:group {"align":"full","style":{"dimensions":{"minHeight":"100px"}},"backgroundColor":"contrast","className":"is-style-pull-100","layout":{"type":"constrained"}} -->
-	<div class="wp-block-group alignfull is-style-pull-100 has-contrast-background-color has-background" style="min-height:100px"></div>
+	<!-- wp:group {"align":"full","style":{"dimensions":{"minHeight":"80px"},"spacing":{"margin":{"top":"-80px"}}},"backgroundColor":"contrast","layout":{"type":"constrained"}} -->
+	<div class="wp-block-group alignfull has-contrast-background-color has-background" style="min-height:80px;margin-top:-80px"></div>
 	<!-- /wp:group -->
 </main>
 <!-- /wp:group -->
