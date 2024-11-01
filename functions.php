e10dff569559b83243acaf8eb62f45159c897647
Brian Gardner <bmg1227@gmail.com>
2024-11-01 15:07:34 -0700

    Add function to theme for auto-updates. closes #43

--- functions.php
+++ functions.php
@@ -105,3 +105,35 @@ function powder_register_pattern_categories() {
 	}
 }
 add_action( 'init', 'powder_register_pattern_categories' );
+
+/**
+ * Check for theme updates.
+ */
+function powder_theme_updates( $transient ) {
+    $update_url = 'https://powderwp.com/updates.json';
+
+    $response = wp_remote_get( $update_url );
+    if ( is_wp_error( $response ) ) {
+        return $transient;
+    }
+
+    $data = json_decode( wp_remote_retrieve_body( $response ) );
+    if ( ! $data ) {
+        return $transient;
+    }
+
+    $theme = wp_get_theme( 'powder' );
+    $current_version = $theme->get( 'Version' );
+
+    if ( version_compare( $current_version, $data->version, '<' ) ) {
+        $transient->response['powder'] = array(
+            'theme'       => 'powder',
+            'new_version' => $data->version,
+            'url'         => 'https://powderwp.com/changelog/',
+            'package'     => $data->download_url,
+        );
+    }
+
+    return $transient;
+}
+add_filter( 'pre_set_site_transient_update_themes', 'powder_theme_updates' );
