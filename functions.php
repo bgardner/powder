dfab1c05bd8902ab1860ff7a1fe5581be2d8f9e8
Brian Gardner <bmg1227@gmail.com>
2024-11-03 16:11:24 -0700

    Remove gradient cover block style. #45

--- functions.php
+++ functions.php
@@ -45,9 +45,6 @@ function powder_register_block_styles() {
 		'core/columns' => array(
 			'column-reverse' => __( 'Reverse', 'powder' ),
 		),
-		'core/cover' => array(
-			'gradient' => __( 'Gradient', 'powder' ),
-		),
 		'core/list' => array(
 			'no-style' => __( 'No Style', 'powder' ),
 		),
