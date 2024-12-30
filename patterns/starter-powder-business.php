e3a3e560041b9800b5fefa5c16626f0cb9f88c1e
Brian Gardner <bmg1227@gmail.com>
2024-12-30 09:40:47 -0700

    Add Starter patterns.

---
+++ patterns/starter-powder-business.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Starter content for Powder Business
+ * Slug: powder/starter-powder-business
+ * Categories: powder-starter
+ */
+?>
+<!-- wp:pattern {"slug":"powder/hero-cover-stack-dark"} /-->
+<!-- wp:pattern {"slug":"powder/hero-columns-image-text"} /-->
+<!-- wp:pattern {"slug":"powder/call-to-action-stacked-dark"} /-->
+<!-- wp:pattern {"slug":"powder/content-feature-columns"} /-->
+<!-- wp:pattern {"slug":"powder/hero-columns-text-image-dark"} /-->
---
+++ patterns/starter-powder-social.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Starter content for Powder Social
+ * Slug: powder/starter-powder-social
+ * Categories: powder-starter
+ */
+?>
+<!-- wp:pattern {"slug":"powder/hero-stacked-text-image-dark"} /-->
+<!-- wp:pattern {"slug":"powder/hero-columns-text-image"} /-->
+<!-- wp:pattern {"slug":"powder/content-social-numbers-dark"} /-->
+<!-- wp:pattern {"slug":"powder/hero-columns-image-text"} /-->
+<!-- wp:pattern {"slug":"powder/content-logos-dark"} /-->
+<!-- wp:pattern {"slug":"powder/hero-columns-text-image"} /-->
