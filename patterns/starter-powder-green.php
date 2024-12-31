e78e5c11101ecf414da793bbba24cc27c52dc2e5
Brian Gardner <bmg1227@gmail.com>
2024-12-31 10:38:26 -0700

    Add Starter pattern.

---
+++ patterns/starter-powder-green.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: Starter content for Powder Green
+ * Slug: powder/starter-powder-green
+ * Categories: powder-starter
+ */
+?>
+<!-- wp:pattern {"slug":"powder/content-about-half"} /-->
+<!-- wp:pattern {"slug":"powder/gallery-full-width"} /-->
+<!-- wp:pattern {"slug":"powder/content-feature-about"} /-->
