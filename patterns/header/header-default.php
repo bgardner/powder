e8cb0f4b17c61e1e73a0281e7cc6fc5829bc5067
Brian Gardner <brian@briangardner.com>
2025-04-27 18:10:33 -0700

    Better spacing in Header pattern.

--- patterns/header/header-default.php
+++ patterns/header/header-default.php
@@ -8,7 +8,7 @@
 ?>
 <!-- wp:group {"metadata":{"name":"Header"},"align":"full","style":{"spacing":{"margin":{"top":"0px"},"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"layout":{"inherit":true,"type":"constrained"}} -->
 <div class="wp-block-group alignfull" style="margin-top:0px;padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)">
-<!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
+	<!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
 	<div class="wp-block-group alignwide">
 		<!-- wp:site-title {"level":0} /-->
 		<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true}} /-->
