7dc3be4ae41ac5378257aa61b8ea8f2b630a4c04
Brian Gardner <brian@briangardner.com>
2025-05-18 07:38:05 -0700

    Reset Posts patterns, add colors.

---
+++ patterns/posts/posts-grid-contrast.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Title: Grid of posts
+ * Slug: powder/posts-grid-contrast
+ * Categories: powder-posts
+ * Block Types: core/query
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Posts"},"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"contrast","textColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-base-color has-contrast-background-color has-text-color has-background has-link-color">
+	<!-- wp:query {"queryId":0,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"align":"wide","layout":{"type":"default"}} -->
+	<div class="wp-block-query alignwide">
+		<!-- wp:post-template {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"grid","columnCount":3}} -->
+			<!-- wp:group {"tagName":"article","layout":{"type":"default"}} -->
+			<article class="wp-block-group">
+			<!-- wp:post-featured-image {"isLink":true,"aspectRatio":"16/9"} /-->
+				<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20","margin":{"top":"var:preset|spacing|30"}}}} -->
+				<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--30)">
+					<!-- wp:group {"tagName":"header","style":{"spacing":{"blockGap":"10px"}},"className":"entry-header","layout":{"type":"default"}} -->
+					<header class="wp-block-group entry-header">
+						<!-- wp:post-title {"isLink":true,"fontSize":"large"} /-->
+						<!-- wp:pattern {"slug":"powder/post-meta"} /-->
+					</header>
+					<!-- /wp:group -->
+					<!-- wp:post-excerpt {"moreText":"Read More →","excerptLength":20,"style":{"spacing":{"margin":{"top":"var:preset|spacing|20"}}},"fontSize":"small"} /-->
+				</div>
+				<!-- /wp:group -->
+			</article>
+			<!-- /wp:group -->
+		<!-- /wp:post-template -->
+		<!-- wp:group {"style":{"spacing":{"margin":{"top":"0"}}},"layout":{"type":"default"}} -->
+		<div class="wp-block-group" style="margin-top:0">
+			<!-- wp:query-pagination {"layout":{"type":"flex","justifyContent":"space-between"}} -->
+				<!-- wp:query-pagination-previous /-->
+				<!-- wp:query-pagination-next /-->
+			<!-- /wp:query-pagination -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:query -->
+</div>
+<!-- /wp:group -->
