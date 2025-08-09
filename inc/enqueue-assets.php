e6888c18dec98318479c70d7d956b40e478e2683
Brian Gardner <brian@briangardner.com>
2025-08-08 21:01:33 -0700

    Add Header effect.

--- inc/enqueue-assets.php
+++ inc/enqueue-assets.php
@@ -4,5 +4,6 @@
  */
 function powder_enqueue_style_sheet() {
 	wp_enqueue_style( 'powder', get_template_directory_uri() . '/style.css', array(), wp_get_theme( 'powder' )->get( 'Version' ) );
+	wp_enqueue_script( 'powder-header', get_template_directory_uri() . '/assets/js/scroll-header.js', array(), null, true );
 }
 add_action( 'wp_enqueue_scripts', 'powder_enqueue_style_sheet' );
