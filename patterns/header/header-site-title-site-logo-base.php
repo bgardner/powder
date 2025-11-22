405aec529e13b46da7733ee8bdd211b2cff68275
Brian Gardner <brian@briangardner.com>
2025-11-22 10:49:46 -0700

    Better Header pattern spacing.

--- patterns/header/header-site-title-site-logo-base.php
+++ patterns/header/header-site-title-site-logo-base.php
@@ -10,10 +10,10 @@
 <div class="wp-block-group alignfull has-base-background-color has-background" style="margin-top:0px;padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)">
 <!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
 	<div class="wp-block-group alignwide">
-		<!-- wp:group {"style":{"spacing":{"blockGap":"10px"},"layout":{"selfStretch":"fixed","flexSize":"200px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+		<!-- wp:group {"style":{"spacing":{"blockGap":"15px"},"layout":{"selfStretch":"fixed","flexSize":"200px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 		<div class="wp-block-group">
 			<!-- wp:site-logo {"width":30} /-->
-			<!-- wp:group {"style":{"border":{"left":{"width":"1px","style":"dotted"}},"spacing":{"padding":{"left":"15px"}}},"layout":{"type":"constrained"}} -->
+			<!-- wp:group {"style":{"border":{"left":{"width":"1px","style":"dotted"}},"spacing":{"padding":{"left":"12px"}}},"layout":{"type":"constrained"}} -->
 			<div class="wp-block-group" style="border-left-style:dotted;border-left-width:1px;padding-left:15px">
 				<!-- wp:site-title {"level":0,"style":{"typography":{"textTransform":"uppercase"}},"fontSize":"medium"} /-->
 			</div>
