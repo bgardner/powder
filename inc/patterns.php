f5cd9ec64dfdab40cbf3981ad71a779c7a10c76e
Brian Gardner <brian@briangardner.com>
2025-12-30 08:48:58 -0700

    Remove pattern category registration.

--- inc/patterns.php
+++ inc/patterns.php
@@ -9,55 +9,23 @@ add_action( 'init', function() {
 
 	// Define pattern categories.
 	$categories = [
-		'call-to-action' => [
-			'Call to Action',
-			'A collection of call to action patterns for Powder.',
-		],
-		'content' => [
-			'Content',
-			'A collection of content patterns for Powder.',
-		],
-		'example' => [
-			'Example',
-			'A collection of example patterns for Powder.',
-		],
 		'footer' => [
-			'Footer',
-			'A collection of footer patterns for Powder.',
-		],
-		'gallery' => [
-			'Gallery',
-			'A collection of gallery patterns for Powder.',
+			'label'       => __( 'Footer', 'powder' ),
+			'description' => __( 'A collection of footer patterns for Powder.', 'powder' ),
 		],
 		'header' => [
-			'Header',
-			'A collection of header patterns for Powder.',
-		],
-		'hero' => [
-			'Hero',
-			'A collection of hero patterns for Powder.',
-		],
-		'pricing' => [
-			'Pricing',
-			'A collection of pricing patterns for Powder.',
-		],
-		'team' => [
-			'Team',
-			'A collection of team patterns for Powder.',
-		],
-		'testimonials' => [
-			'Testimonials',
-			'A collection of testimonial patterns for Powder.',
+			'label'       => __( 'Header', 'powder' ),
+			'description' => __( 'A collection of header patterns for Powder.', 'powder' ),
 		],
 	];
 
 	// Register each category.
-	foreach ( $categories as $slug => [ $label, $description ] ) {
+	foreach ( $categories as $slug => $data ) {
 		register_block_pattern_category(
 			'powder-' . $slug,
 			[
-				'label'       => $label,
-				'description' => $description,
+				'label'       => $data['label'],
+				'description' => $data['description'],
 			]
 		);
 	}
