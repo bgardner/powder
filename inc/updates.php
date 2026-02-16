90acb3a0b9706879632f53112b02a2e463e00630
Brian Gardner <brian@briangardner.com>
2026-02-15 19:23:22 -0700

    Better transient for updates.

--- inc/updates.php
+++ inc/updates.php
@@ -6,21 +6,26 @@
  */
 
 function powder_theme_updates( $transient ) {
-	$update_url = 'https://powder.design/powder-theme-updates.json';
+	$cache_key = 'powder_update_check';
+	$data      = get_transient( $cache_key );
 
-	$response = wp_remote_get( $update_url );
-	if ( is_wp_error( $response ) ) {
-		return $transient;
+	if ( false === $data ) {
+		$response = wp_remote_get( 'https://powder.design/powder-theme-updates.json' );
+
+		if ( is_wp_error( $response ) ) {
+			return $transient;
+		}
+
+		$data = json_decode( wp_remote_retrieve_body( $response ) );
+
+		if ( ! $data ) {
+			return $transient;
+		}
+
+		set_transient( $cache_key, $data, 12 * HOUR_IN_SECONDS );
 	}
 
-	$data = json_decode( wp_remote_retrieve_body( $response ) );
-	if ( ! $data ) {
-		return $transient;
-	}
-
-	$current_version = powder_version();
-
-	if ( version_compare( $current_version, $data->version, '<' ) ) {
+	if ( version_compare( powder_version(), $data->version, '<' ) ) {
 		$transient->response['powder'] = array(
 			'theme'       => 'powder',
 			'new_version' => $data->version,
@@ -32,3 +37,8 @@ function powder_theme_updates( $transient ) {
 	return $transient;
 }
 add_filter( 'pre_set_site_transient_update_themes', 'powder_theme_updates' );
+
+function powder_clear_update_cache() {
+	delete_transient( 'powder_update_check' );
+}
+add_action( 'load-update-core.php', 'powder_clear_update_cache' );
