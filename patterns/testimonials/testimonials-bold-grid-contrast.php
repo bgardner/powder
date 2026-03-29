71dcf3c5b3bed08f9ebfd4c7f3d72fd1fdd4797d
Brian Gardner <brian@briangardner.com>
2026-03-29 15:31:46 -0700

    Add Testimonials patterns.

---
+++ patterns/testimonials/testimonials-bold-grid-contrast.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Title: Bold testimonial with grid of text
+ * Slug: powder/testimonials-bold-grid-contrast
+ * Categories: powder-testimonials
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Testimonials"},"align":"full","style":{"spacing":{"margin":{"top":"0"},"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"},"blockGap":"var:preset|spacing|20"}},"backgroundColor":"contrast","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-contrast-background-color has-background" style="margin-top:0;padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)">
+	<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|30","padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|80","right":"var:preset|spacing|80"}},"typography":{"lineHeight":"1.5"}},"backgroundColor":"base","layout":{"type":"default"}} -->
+	<div class="wp-block-group alignwide has-base-background-color has-background" style="padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80);line-height:1.5">
+		<!-- wp:image {"width":"60px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
+		<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/icons/icon-quote-bottom.svg'; ?>" alt="Quote bottom icon" style="width:60px"/></figure>
+		<!-- /wp:image -->
+		<!-- wp:paragraph {"className":"is-style-balanced","style":{"typography":{"textAlign":"center","lineHeight":"1.25"}},"fontSize":"x-large"} -->
+		<p class="has-text-align-center is-style-balanced has-x-large-font-size" style="line-height:1.25"><?php echo esc_html__( 'Powder is the premier block theme for WordPress designers and agencies, offering an extensive library of patterns.', 'powder' ); ?></p>
+		<!-- /wp:paragraph -->
+		<!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase","letterSpacing":"0.05em","fontStyle":"normal","fontWeight":"500","textAlign":"center"}},"fontSize":"x-small"} -->
+		<p class="has-text-align-center has-x-small-font-size" style="font-style:normal;font-weight:500;letter-spacing:0.05em;text-transform:uppercase"><?php echo esc_html__( 'Jennifer Kayne', 'powder' ); ?></p>
+		<!-- /wp:paragraph -->
+		<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0"}},"typography":{"textAlign":"center"}},"fontSize":"xx-small"} -->
+		<p class="has-text-align-center has-xx-small-font-size" style="margin-top:0"><?php echo esc_html__( 'Founder &amp; Creative Director', 'powder' ); ?></p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->
+	<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"grid","minimumColumnWidth":"480px"}} -->
+	<div class="wp-block-group alignwide">
+		<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|20","padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|80","right":"var:preset|spacing|80"}},"typography":{"lineHeight":"1.5"}},"backgroundColor":"base","layout":{"type":"default"}} -->
+		<div class="wp-block-group alignwide has-base-background-color has-background" style="padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80);line-height:1.5">
+			<!-- wp:image {"width":"48px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
+			<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/icons/icon-quote-bottom.svg'; ?>" alt="Quote bottom icon" style="width:48px"/></figure>
+			<!-- /wp:image -->
+			<!-- wp:paragraph {"className":"is-style-balanced","style":{"typography":{"textAlign":"center"}},"fontSize":"x-small"} -->
+			<p class="has-text-align-center is-style-balanced has-x-small-font-size"><?php echo esc_html__( 'Powder is the premier block theme for WordPress designers and agencies, offering an extensive library of patterns.', 'powder' ); ?></p>
+			<!-- /wp:paragraph -->
+			<!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase","letterSpacing":"0.05em","fontStyle":"normal","fontWeight":"500","textAlign":"center"}},"fontSize":"x-small"} -->
+			<p class="has-text-align-center has-x-small-font-size" style="font-style:normal;font-weight:500;letter-spacing:0.05em;text-transform:uppercase"><?php echo esc_html__( 'Jennifer Kayne', 'powder' ); ?></p>
+			<!-- /wp:paragraph -->
+			<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0"}},"typography":{"textAlign":"center"}},"fontSize":"xx-small"} -->
+			<p class="has-text-align-center has-xx-small-font-size" style="margin-top:0"><?php echo esc_html__( 'Founder &amp; Creative Director', 'powder' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
+		<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|20","padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|80","right":"var:preset|spacing|80"}},"typography":{"lineHeight":"1.5"}},"backgroundColor":"base","layout":{"type":"default"}} -->
+		<div class="wp-block-group alignwide has-base-background-color has-background" style="padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80);line-height:1.5">
+			<!-- wp:image {"width":"48px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
+			<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/icons/icon-quote-bottom.svg'; ?>" alt="Quote bottom icon" style="width:48px"/></figure>
+			<!-- /wp:image -->
+			<!-- wp:paragraph {"className":"is-style-balanced","style":{"typography":{"textAlign":"center"}},"fontSize":"x-small"} -->
+			<p class="has-text-align-center is-style-balanced has-x-small-font-size"><?php echo esc_html__( 'Powder is the premier block theme for WordPress designers and agencies, offering an extensive library of patterns.', 'powder' ); ?></p>
+			<!-- /wp:paragraph -->
+			<!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase","letterSpacing":"0.05em","fontStyle":"normal","fontWeight":"500","textAlign":"center"}},"fontSize":"x-small"} -->
+			<p class="has-text-align-center has-x-small-font-size" style="font-style:normal;font-weight:500;letter-spacing:0.05em;text-transform:uppercase"><?php echo esc_html__( 'Jennifer Kayne', 'powder' ); ?></p>
+			<!-- /wp:paragraph -->
+			<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0"}},"typography":{"textAlign":"center"}},"fontSize":"xx-small"} -->
+			<p class="has-text-align-center has-xx-small-font-size" style="margin-top:0"><?php echo esc_html__( 'Founder &amp; Creative Director', 'powder' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
