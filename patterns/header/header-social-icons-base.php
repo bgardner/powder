2142c08cb3871ce524e6789a81b6fd2e0fcc808b
Brian Gardner <brian@briangardner.com>
2026-03-29 10:01:08 -0700

    Use block visibility on social icons.

--- patterns/header/header-social-icons-base.php
+++ patterns/header/header-social-icons-base.php
@@ -17,8 +17,8 @@
 		</div>
 		<!-- /wp:group -->
 		<!-- wp:navigation {"overlay":"navigation-overlay","layout":{"type":"flex","setCascadingProperties":true}} /-->
-		<!-- wp:social-links {"openInNewTab":true,"size":"has-small-icon-size","style":{"layout":{"selfStretch":"fixed","flexSize":"200px"},"spacing":{"blockGap":{"left":"5px"}}},"className":"is-style-outline is-style-hidden-mobile","layout":{"type":"flex","justifyContent":"right"}} -->
-		<ul class="wp-block-social-links has-small-icon-size is-style-outline is-style-hidden-mobile">
+		<!-- wp:social-links {"openInNewTab":true,"size":"has-small-icon-size","metadata":{"blockVisibility":{"viewport":{"tablet":false,"mobile":false}}},"className":"is-style-outline","style":{"layout":{"selfStretch":"fixed","flexSize":"200px"},"spacing":{"blockGap":{"left":"5px"}}},"layout":{"type":"flex","justifyContent":"right"}} -->
+		<ul class="wp-block-social-links has-small-icon-size is-style-outline">
 			<!-- wp:social-link {"url":"https://x.com/","service":"x"} /-->
 			<!-- wp:social-link {"url":"https://www.linkedin.com/","service":"linkedin"} /-->
 			<!-- wp:social-link {"url":"https://instagram.com/","service":"instagram"} /-->
