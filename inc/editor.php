3676b437dc7aef93b960509d762e852d0a5a1e00
Brian Gardner <brian@briangardner.com>
2026-04-20 10:12:19 -0700

    Remove sidebar from theme.

--- inc/editor.php
+++ inc/editor.php
@@ -1,21 +1,9 @@
 <?php
 /**
- * Register template parts and block styles.
+ * Register block styles.
  *
  * @package Powder
  */
-
-function powder_template_parts( array $areas ) {
-	$areas[] = [
-		'area'        => 'sidebar',
-		'area_tag'    => 'section',
-		'label'       => __( 'Sidebar', 'powder' ),
-		'description' => __( 'The Sidebar template defines a page area that typically contains secondary content, navigation, or social icons.', 'powder' ),
-		'icon'        => 'sidebar',
-	];
-	return $areas;
-}
-add_filter( 'default_wp_template_part_areas', 'powder_template_parts' );
 
 function powder_register_block_styles() {
 	$block_styles = [
