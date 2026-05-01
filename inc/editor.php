8bfe4ce38ec5d889f55aa1452cb4fdd7bbe1e9eb
Brian Gardner <brian@briangardner.com>
2026-05-01 15:21:18 -0700

    Add Zoom block style.

--- inc/editor.php
+++ inc/editor.php
@@ -13,12 +13,18 @@ function powder_register_block_styles() {
 		'core/heading' => [
 			'balanced' => __( 'Balanced', 'powder' ),
 		],
+		'core/image' => [
+			'zoom' => __( 'Zoom', 'powder' ),
+		],
 		'core/list' => [
 			'no-style' => __( 'No Style', 'powder' ),
 		],
 		'core/paragraph' => [
 			'balanced' => __( 'Balanced', 'powder' ),
 			'eyebrow' => __( 'Eyebrow', 'powder' ),
+		],
+		'core/post-featured-image' => [
+			'zoom' => __( 'Zoom', 'powder' ),
 		],
 		'core/post-title' => [
 			'balanced' => __( 'Balanced', 'powder' ),
