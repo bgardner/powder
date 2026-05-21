881e546c0aad4c24a6ba1a5747323281c876728b
Brian Gardner <brian@briangardner.com>
2026-05-21 15:11:14 -0700

    Minor /inc/ tweaks.

--- functions.php
+++ functions.php
@@ -9,6 +9,7 @@
  */
 
 require get_template_directory() . '/inc/editor.php';
+require get_template_directory() . '/inc/motion.php';
 require get_template_directory() . '/inc/patterns.php';
 require get_template_directory() . '/inc/settings.php';
 require get_template_directory() . '/inc/setup.php';
